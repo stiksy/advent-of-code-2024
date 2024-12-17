0d8b6531062782b1684834a9918ee10a8987810d
Fernando Levi Marquardt <fernando.marquardt@king.com>
2024-12-17 16:02:54 +0000

    Day 14

--- src/main/java/com/stiksy/advent/days/Day14.java
+++ src/main/java/com/stiksy/advent/days/Day14.java
@@ -3,8 +3,10 @@ package com.stiksy.advent.days;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day14 {
+    public static int seconds = 0;
 
     public List<Robot> parseInput(String input) {
         String lines[] = input.split("\n");
@@ -25,6 +27,7 @@ public class Day14 {
             int y = robot.position.y;
             addToMap(map, y, x);
         }
+        printMap(map);
         for (int i = 0; i < times; i++) {
             for (Robot robot : robots) {
                 int x = robot.position.x;
@@ -51,8 +54,26 @@ public class Day14 {
                 }
                 addToMap(map, robot.position.y, robot.position.x);
             }
+            printMap(map);
         }
         return map;
+    }
+
+    private static void printMap(char[][] map) {
+        for (int line = 0; line < map.length; line++) {
+            for (int column = 0; column < map[0].length - 10; column++) {
+                if (map[line][column] != '.' && map[line][column + 1] != '.' && map[line][column + 2] != '.' && map[line][column + 3] != '.' && map[line][column + 4] != '.'
+                        && map[line][column + 5] != '.' && map[line][column + 6] != '.' && map[line][column + 7] != '.' && map[line][column + 8] != '.' && map[line][column + 9] != '.') {
+                    List<String> actualMap = new ArrayList<>();
+                    for (char[] row : map) {
+                        actualMap.add(new String(row));
+                    }
+                    System.out.println("\n\nSeconds: " + seconds);
+                    System.out.println(actualMap.stream().collect(Collectors.joining("\n")));
+                }
+            }
+        }
+        seconds++;
     }
 
     private static void addToMap(char[][] map, int y, int x) {
@@ -62,6 +83,42 @@ public class Day14 {
             int newValue = Integer.parseInt(String.valueOf(map[y][x])) + 1;
             map[y][x] = String.valueOf(newValue).charAt(0);
         }
+    }
+
+    public long getSafetyFactor(char[][] map) {
+        int firstQuadrantRobots = 0;
+        int secondQuadrantRobots = 0;
+        int thirdQuadrantRobots = 0;
+        int fourthQuadrantRobots = 0;
+        for (int line = 0; line < (map.length - 1) / 2; line++) {
+            for (int column = 0; column < (map[0].length - 1) / 2; column++) {
+                if (map[line][column] != '.') {
+                    firstQuadrantRobots += Integer.parseInt(String.valueOf(map[line][column]));
+                }
+            }
+        }
+        for (int line = 0; line < (map.length - 1) / 2; line++) {
+            for (int column = (map[0].length + 1) / 2; column < map[0].length; column++) {
+                if (map[line][column] != '.') {
+                    secondQuadrantRobots += Integer.parseInt(String.valueOf(map[line][column]));
+                }
+            }
+        }
+        for (int line = (map.length / 2) + 1; line < map.length; line++) {
+            for (int column = 0; column < (map[0].length - 1) / 2; column++) {
+                if (map[line][column] != '.') {
+                    thirdQuadrantRobots += Integer.parseInt(String.valueOf(map[line][column]));
+                }
+            }
+        }
+        for (int line = (map.length / 2) + 1; line < map.length; line++) {
+            for (int column = (map[0].length + 1) / 2; column < map[0].length; column++) {
+                if (map[line][column] != '.') {
+                    fourthQuadrantRobots += Integer.parseInt(String.valueOf(map[line][column]));
+                }
+            }
+        }
+        return firstQuadrantRobots * secondQuadrantRobots * thirdQuadrantRobots * fourthQuadrantRobots;
     }
 
     static class Robot {
